3d34786a192a31bec63e59b48185927f61bf8e5c
điệp <hdiep2705@gmail.com>
2021-02-24 20:49:48 +0700

    upcode

---
+++ src/data.js
@@ -0,0 +1,77 @@
+const data =    {
+    products:[
+        {
+            _id: '1',
+            name: 'Xiaomi note 7',
+            category: 'phone',
+            image:'/images/redmi-note-7.jpg',
+            price: 100,
+            brand: 'Xiaomi',
+            rating: 4.5,
+            numReviews: 10,
+            description:'high quality product'
+
+        },
+        {
+            _id: '2',
+            name: 'Xiaomi note 8',
+            category: 'phone',
+            image:'/images/redmi-note-8.jpg',
+            price: 150,
+            brand: 'Xiaomi',
+            rating: 4.0,
+            numReviews: 10,
+            description:'high quality product'
+
+        },
+        {
+            _id: '3',
+            name: 'Xiaomi note 9',
+            category: 'phone',
+            image:'/images/redmi-note-9.jpg',
+            price: 220,
+            brand: 'Xiaomi',
+            rating: 4.8,
+            numReviews: 17,
+            description:'high quality product'
+
+        },
+        {
+            _id: '4',
+            name: 'Xiaomi note 10',
+            category: 'phone',
+            image:'/images/redmi-note-10.jpg',
+            price: 350,
+            brand: 'Xiaomi',
+            rating: 4.5,
+            numReviews: 14,
+            description:'high quality product'
+
+        },
+        {
+            _id: '5',
+            name: 'Xiaomi note 11',
+            category: 'phone',
+            image:'/images/redmi-note-11.jpg',
+            price: 240,
+            brand: 'Xiaomi',
+            rating: 4.5,
+            numReviews: 10,
+            description:'high quality product'
+
+        },
+        {
+            _id: '6',
+            name: 'Xiaomi mi 8',
+            category: 'phone',
+            image:'/images/redmi-mi-8.jpg',
+            price: 139,
+            brand: 'Xiaomi',
+            rating: 4.5,
+            numReviews: 15,
+            description:'high quality product'
+
+        }, 
+    ]
+}
+export default data;
